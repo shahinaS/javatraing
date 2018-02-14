83e4168e3c0f86c13f1358a3107e2e4eec4a2b6e
sheena <shena@mayalogic.com>
2018-02-14 18:18:27 +0500

    Modified Hello World App

---
+++ src/com/maya/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.maya.test;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("I am going to add code");
+
+    }
+}
